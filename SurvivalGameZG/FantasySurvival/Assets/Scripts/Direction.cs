b5e6aef73e8f4ed3193dc705f4e538e1e9ed85de
gary <garylucasgaming@gmail.com>
2022-07-08 00:32:09 -0400

    section 1 of videos done
    hell yeah

---
+++ SurvivalGameZG/FantasySurvival/Assets/Scripts/Direction.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public enum Direction
+{
+    foreward,  // z+ direction
+    right,  // +x direction
+    backwards,   // -z direction
+    left,   // -x direction
+    up,     // +y direction
+    down    // -y direction
+};
---
+++ SurvivalGameZG/FantasySurvival/Assets/Scripts/DirectionExtensions.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DirectionExtensions
+{
+    public static Vector3Int GetVector(this Direction direction)
+    {
+         switch(direction)
+        {
+            case Direction.up: return Vector3Int.up;
+            case Direction.down: return Vector3Int.down;
+            case Direction.right:  return Vector3Int.right;
+           case Direction.left:  return Vector3Int.left;
+           case Direction.foreward: return Vector3Int.forward;
+           case Direction.backwards: return Vector3Int.back;
+            default: throw new Exception("invalid input direction");
+        }
+
+
+}
+}
